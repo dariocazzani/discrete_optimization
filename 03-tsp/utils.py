2ef62d4c9b58e5a2119b7d9a33dd134df1ac675b
Dario Cazzani <dcazzani@cisco.com>
2017-07-03 11:25:53 -0700

    Implementation of progress bar

--- 03-tsp/utils.py
+++ 03-tsp/utils.py
@@ -1,4 +1,15 @@
 import math
+import sys
 
 def distance(point1, point2):
     return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
+
+def progress(count, total, suffix=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stderr.write('[%s] %s%s ...%s\r' % (bar, percents, '%', suffix))
+    sys.stdout.flush
