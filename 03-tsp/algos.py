b4a7a196d543ff62e64646069f48c0ce2776f5b8
Dario Cazzani <dcazzani@cisco.com>
2017-07-03 11:25:39 -0700

    Added progress bar and simple plotting of all points

--- 03-tsp/algos.py
+++ 03-tsp/algos.py
@@ -1,9 +1,10 @@
 from __future__ import division
 
-from utils import distance
+from utils import distance, progress
 import numpy as np
 import copy
 import operator
+import matplotlib.pyplot as plt
 
 def find_distances(origin, points):
     distances = {}
@@ -17,13 +18,22 @@ def find_point_by_index(next_point, points):
         if point.index == next_point:
             return index
 
+def plot_points(points):
+    for point in points:
+        plt.scatter(point.x, point.y, alpha=0.5)
+    plt.show()
+
 def greedy_solver(points):
     new_points = []
     old_points = copy.deepcopy(points)
-    next_point = np.random.randint(0, len(points))
+    next_point = 0#np.random.randint(0, len(points))
     current_point = old_points.pop(next_point)
     new_points.append(current_point)
+    counter = 0
+    total_points = len(old_points)
     while old_points:
+        progress(counter, total_points)
+        counter += 1
         # find distances from current point to all other available points
         distances = find_distances(current_point, old_points)
         # select the shortest distance
@@ -32,14 +42,16 @@ def greedy_solver(points):
         index = find_point_by_index(next_point, old_points)
         current_point= old_points.pop(index)
         new_points.append(current_point)
-        # reorder points py index
         # print(distances)
+    print('')
+
+    plot_points(points)
 
     solution = []
     for point in new_points:
         solution.append(point.index)
 
-    return solution, new_points
+    return solution, points
 
 
 def trivial_solver(points):
